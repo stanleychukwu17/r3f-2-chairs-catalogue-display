b036178f4d7c844b658aa9ba967edbb249dac5cb
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-10-18 12:57:28 +0100

    moved the lightComp and modelComp to separate components also looped through the .gltf files to display the chairs dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
 import * as THREE from 'three'
-import { useRef } from "react"
-import { Canvas, useFrame } from "@react-three/fiber"
-import {Html, useGLTF} from "@react-three/drei"
+import { useRef, useState, useEffect} from "react"
+import { Canvas, useFrame, useThree } from "@react-three/fiber"
+import {Html} from "@react-three/drei"
+import { useMotionValueEvent, useScroll } from 'framer-motion'
 
-import Section from "./components/Section/Section"
+import LightsComp from "./components/App-Components/LightsComp"
+import ModelComp from "./components/App-Components/ModelComp"
+// import Section from "./components/Section/Section"
 import Header from "./components/Header/Header"
 import './App.css'
 
-const LightsComp = () => {
-  return (
-    <>
-      <ambientLight intensity={0.5} />
-      <directionalLight intensity={Math.PI/2} position={[10, 10, 5]} />
-      <directionalLight intensity={Math.PI/2} position={[-10, 10, 0]} />
-    </>
-  )
+const chairs: Record<string, {title: string, path: string}> = {
+  "yellow": {
+    title:"yellow", path:"/yellow.gltf"
+  },
+  "grey": {
+    title:"grey", path:"/grey.gltf"
+  },
+  // "limegreen": {
+  //   title:"limegreen", path:"/lime_green.gltf"
+  // },
+  // "pink": {
+  //   title:"pink", path:"/candy_pink.gltf"
+  // },
 }
 
 
-type ModelCompProps = {
-  modelPath: string
-}
-const ModelComp = ({modelPath}: ModelCompProps) => {
-  const gltf = useGLTF(modelPath, true)
-  return (
-    <primitive object={gltf.scene} />
-  )
-}
 
 
 type HtmlCompProps = {
@@ -42,12 +41,48 @@ const HtmlComponent = ({children, modelPath, positionY}: HtmlCompProps) => {
   })
 
   return(
-    <Section offset={0} factor={1}>
       <group ref={groupRef} position={[0, positionY, 0]}>
         <ModelComp modelPath={modelPath} />
         {children}
       </group>
-    </Section>
+  )
+}
+
+const TitleComp = ({title, offset}: {title: string, offset: number}) => {
+  const containerRef = useRef<HTMLDivElement>(null!);
+  const {scrollY} = useScroll({target: containerRef})
+  const pageOffset = offset
+  const [pageHeight, setPageHeight] = useState(0);
+  const pageDistanceFromTheTop = pageOffset * pageHeight;
+
+  const mySection = () => {
+    const pageRange = {start: pageDistanceFromTheTop, end: pageDistanceFromTheTop + pageHeight, currentPage: ''}
+
+    if (scrollY.get() >= pageRange.start && scrollY.get() <= pageRange.end) {
+      pageRange.currentPage = title;
+      console.log('now in current page', title)
+    }
+  
+    return pageRange
+  }
+
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setPageHeight(containerRef.current.getBoundingClientRect().height);
+    }
+  }, []);
+
+
+  console.log({pageHeight, title, pageOffset, pageDistanceFromTheTop})
+  useMotionValueEvent(scrollY, "change", (value) => {
+    console.log({scrolled: Math.round(value), title, pageDistanceFromTheTop, "mySection": mySection()})
+  })
+
+  return (
+    <div className='others' ref={containerRef}>
+      <p>{title}</p>
+    </div>
   )
 }
 
@@ -56,19 +91,35 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      <Canvas
-        camera={{
-          // near: 0.1, far: 2000,
-          position: [0, 0, 120], fov: 70
-        }}
-      >
-        <LightsComp />
-        <HtmlComponent modelPath="/grey.gltf" positionY={-20}>
-          <Html fullscreen>
-            <div className="title">Hello</div>
-          </Html>
-        </HtmlComponent>
-      </Canvas>
+      <div className="three_cvr">
+        <div className="three_Canvas">
+          <Canvas
+            camera={{
+              // near: 0.1, far: 2000,
+              position: [0, 0, 120], fov: 70
+            }}
+          >
+            <LightsComp />
+
+            {Object.keys(chairs).map((key, index) => {
+              const chair = chairs[key].path
+              const title = chairs[key].title
+
+              return (
+                <HtmlComponent modelPath={chair} positionY={-20 -(index * 60)}  key={key}>
+                  <Html fullscreen>
+                    <div className="title">{title}</div>
+                  </Html>
+                </HtmlComponent>
+              )
+            })}
+          </Canvas>
+        </div>
+
+        {Object.keys(chairs).map((key, index) => {
+          return <TitleComp title={chairs[key].title} offset={index} key={key} />
+        })}
+      </div>
     </div>
   )
 }
