ba7b641bb07ea9bc5f2041bc87ce5815b97b6619
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-10-21 10:50:26 +0100

    Update Zustand store to include window resize state management
    - Added `windowResized` boolean and `setWindowResized` function to Store type
    - Initialized `windowResized` as false in the store
    - Implemented `setWindowResized` to update the window resize state
    - Refactored store creation to use `set` and `get` for better state management
    - Removed console.log from `setPageDetails` for cleaner code

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,16 +11,23 @@ type PageDetailsMap = {
 };
 
 type Store = {
+  windowResized: boolean,
+  setWindowResized: (value:boolean) => void;
+
   pageDetails: PageDetailsMap;
   setPageDetails: (page:number, scrollPercentage: number) => void
 };
 
-export const useStore = create<Store>(() => ({
-  pageDetails: { 100: {page: -1, scrollPercentage: 0} },
+export const useStore = create<Store>((set, get) => ({
+  windowResized: false,
+  setWindowResized: (value:boolean) => set({windowResized: value}),
+
+  pageDetails: {
+    100: {page: -1, scrollPercentage: 0}
+  },
   setPageDetails: (page:number, scrollPercentage: number) => {
     const pageDetails = useStore.getState().pageDetails
     pageDetails[page] = {page, scrollPercentage}
-    console.log("updated page store details", {page, scrollPercentage})
   }
 }));
 
